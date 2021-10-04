f8ca3a01c93e232f7a2604e39833da1dc37ec38a
Ricky Boyd <rickyboyd23@gmail.com>
2021-10-04 14:33:37 -0400

    added js

--- motivate.js
+++ motivate.js
@@ -1,1 +1,51 @@
-console.log('Hello World')
+const BASE_URL = 'https://type.fit/api/quotes';
+
+const getMotivated = () => {
+
+  fetch(`${BASE_URL}`)
+    .then((res) => { return res.json() })
+    .then((resJSON) => {
+
+      quoteList(resJSON);
+
+  })
+    .catch((err) => {
+    console.error(`ERROR: ${err}`)
+  })
+  console.log(quoteList);
+}
+
+const quoteList = (showQuotes) => {
+  const display = document.querySelector('.output');
+  display.innerHTML = " ";
+  
+  for (let i = 0; i < showQuotes.length; i++) {
+    
+    const quote = document.createElement('h3');
+    quote.innerText = showQuotes[i].text;
+    document.querySelector('.output').appendChild(quote)
+
+    const authors = document.createElement('h4');
+    authors.innerText = showQuotes[i].author;
+    document.querySelector('.output').appendChild(authors)
+
+  }
+  
+}
+
+const submitButton = document.querySelector('#submit');
+// console.log(submitButton);
+
+submitButton.addEventListener('click', (ev) => {
+  
+  ev.preventDefault();
+  console.log('CLICKED');
+
+  let clicked = document.querySelector('#submit').value;
+
+  getMotivated(clicked);
+  document.querySelector('#submit').value = ''
+
+});
+
+
