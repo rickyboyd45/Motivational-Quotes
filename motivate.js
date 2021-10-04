818e73f32c1ee1954270184190812796ae29d09d
Ricky Boyd <rickyboyd23@gmail.com>
2021-10-04 19:13:33 -0400

    code swap

--- motivate.js
+++ motivate.js
@@ -3,20 +3,20 @@ const BASE_URL = 'https://type.fit/api/quotes';
 let text = document.querySelector('.text');
 let author = document.querySelector('.author');
 let submitButton = document.querySelector('#submit');
-let no, rem;
+let quotes, mix;
 
 
 
 const newQuote = () => {
-  no = rem;
-  rem = Math.floor(Math.random() * 1600);
+  quotes = mix;
+  mix = Math.floor(Math.random() * 1600);
 
   fetch(`${BASE_URL}`)
     .then((res) => { return res.json() })
     .then((data) => {
       
-      text.innerHTML = data[rem].text;
-      author.innerHTML = data[rem].author;
+      text.innerHTML = data[mix].text;
+      author.innerHTML = data[mix].author;
   })
     .catch((err) => {
     console.error(`ERROR: ${err}`)
