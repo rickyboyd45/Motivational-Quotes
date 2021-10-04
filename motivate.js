b1f825269bacfb1dabc9fde9bfeccc27db6d06f6
Ricky Boyd <rickyboyd23@gmail.com>
2021-10-04 17:21:38 -0400

    changed code to randomize

--- motivate.js
+++ motivate.js
@@ -1,51 +1,61 @@
 const BASE_URL = 'https://type.fit/api/quotes';
 
-const getMotivated = () => {
+let text = document.querySelector('.text');
+let author = document.querySelector('.author');
+let submitButton = document.querySelector('#submit');
+let no, rem;
+
+
+
+const newQuote = () => {
+  no = rem;
+  rem = Math.floor(Math.random() * 100);
 
   fetch(`${BASE_URL}`)
     .then((res) => { return res.json() })
-    .then((resJSON) => {
-
-      quoteList(resJSON);
-
+    .then((data) => {
+      // quoteList(resJSON);
+      text.innerHTML = data[rem].text;
+      author.innerHTML = data[rem].author;
   })
     .catch((err) => {
     console.error(`ERROR: ${err}`)
   })
-  console.log(quoteList);
+  // console.log(quoteList);
 }
 
-const quoteList = (showQuotes) => {
-  const display = document.querySelector('.output');
-  display.innerHTML = " ";
+submitButton.addEventListener("click", newQuote)
+// const quoteList = (showQuotes) => {
+//   const display = document.querySelector('.output');
+//   display.innerHTML = " ";
   
-  for (let i = 0; i < showQuotes.length; i++) {
+//   for (let i = 0; i < 5; i++) {
     
-    const quote = document.createElement('h3');
-    quote.innerText = showQuotes[i].text;
-    document.querySelector('.output').appendChild(quote)
+//     const quote = document.createElement('h3');
+//     quote.innerText = showQuotes[i].text;
+//     document.querySelector('.output').appendChild(quote)
 
-    const authors = document.createElement('h4');
-    authors.innerText = showQuotes[i].author;
-    document.querySelector('.output').appendChild(authors)
+//     const authors = document.createElement('h4');
+//     authors.innerText = showQuotes[i].author;
+//     document.querySelector('.output').appendChild(authors)
 
-  }
+//   }
   
-}
+// }
 
-const submitButton = document.querySelector('#submit');
-// console.log(submitButton);
+// const submitButton = document.querySelector('#submit');
+// // console.log(submitButton);
 
-submitButton.addEventListener('click', (ev) => {
+// submitButton.addEventListener('click', (ev) => {
   
-  ev.preventDefault();
-  console.log('CLICKED');
+//   ev.preventDefault();
+//   console.log('CLICKED');
 
-  let clicked = document.querySelector('#submit').value;
+//   let clicked = document.querySelector('#submit').value;
 
-  getMotivated(clicked);
-  document.querySelector('#submit').value = ''
+//   getMotivated(clicked);
+//   document.querySelector('#submit').value = ''
 
-});
+// });
 
 
